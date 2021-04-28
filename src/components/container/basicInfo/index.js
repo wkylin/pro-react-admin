6577d013b2d4ec08d1f943bbd107cb2ccde5c563
wkylin <wkylin.w@gmail.com>
2021-04-28 10:57:01 +0800

    style: style

--- src/components/container/basicInfo/index.js
+++ src/components/container/basicInfo/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Pagination } from 'antd'
 const BasicInfo = () => {
+  console.log('test')
   return (
     <>
       <Pagination defaultCurrent={6} total={500} />
