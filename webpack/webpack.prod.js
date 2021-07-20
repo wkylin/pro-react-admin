c4be870879b0740507800505b88c4be511f8d223
wkylin <wkylin.w@gmail.com>
2021-07-20 12:06:15 +0800

    build: webpack plugin

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,20 +1,32 @@
+const path = require('path')
+const webpack = require('webpack')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const glob = require('glob')
+const PurgeCSSPlugin = require('purgecss-webpack-plugin')
 
 const regVendor = /[\\/]node_modules[\\/](axios|classnames|)[\\/]/
-const regReact = /[\\/]node_modules[\\/](react|react-dom|react-redux|react-router-config|react-router-dom|react-router-redux|redux)[\\/]/
+const regReact =
+  /[\\/]node_modules[\\/](react|react-dom|react-redux|react-router-config|react-router-dom|react-router-redux|redux)[\\/]/
 
 module.exports = merge(common, {
   mode: 'production',
-  // devtool: false,
+  devtool: false,
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'static/css/[name].[contenthash].css',
       chunkFilename: 'static/css/[name].[contenthash].css',
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${path.resolve(__dirname, '../src')}/**/*.{js, jsx,tsx,scss,less,css}`, { nodir: true }),
+    }),
+    new webpack.BannerPlugin({
+      raw: true,
+      banner: '/** @preserve Powered by promotion-web (https://wkylin.github.io/promotion-web) */',
     }),
   ],
   optimization: {
