b8fd85734b699b3a3c45e7d629da97aab5edd6c1
wkylin <wkylin.w@gmail.com>
2021-09-09 20:44:08 +0800

    build: webpack build config

--- webpack/dev.server.js
+++ webpack/dev.server.js
@@ -3,26 +3,29 @@ const WebpackDevServer = require('webpack-dev-server')
 const portfinder = require('portfinder')
 const config = require('./webpack.dev.server')
 
-const devServerConfig = new WebpackDevServer(webpack(config), {
-  historyApiFallback: true,
-  compress: true,
-  open: true,
-  proxy: {
-    '/': {
-      target: 'https://my-json-server.typicode.com',
-      pathRewrite: { '^/': '' },
-      secure: false,
-      changeOrigin: true,
-    },
-    '/faker': {
-      target: 'http://localhost:4000',
-      pathRewrite: { '^/faker': '' },
-      secure: false,
-      changeOrigin: true,
-      cookieDomainRewrite: 'localhost',
+const devServerConfig = new WebpackDevServer(
+  {
+    historyApiFallback: true,
+    compress: true,
+    open: true,
+    proxy: {
+      '/': {
+        target: 'https://my-json-server.typicode.com',
+        pathRewrite: { '^/': '' },
+        secure: false,
+        changeOrigin: true,
+      },
+      '/faker': {
+        target: 'http://localhost:4000',
+        pathRewrite: { '^/faker': '' },
+        secure: false,
+        changeOrigin: true,
+        cookieDomainRewrite: 'localhost',
+      },
     },
   },
-})
+  webpack(config)
+)
 
 portfinder.getPort(
   {
