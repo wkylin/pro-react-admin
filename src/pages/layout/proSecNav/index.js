726ebc40de98045b4ea9beab2cc9952f1392a427
wkylin.w <wkylin.w@gmial.com>
2022-01-23 15:46:09 +0800

    feat: menu tabs

--- src/pages/layout/proSecNav/index.js
+++ src/pages/layout/proSecNav/index.js
@@ -1,48 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Menu } from 'antd'
-// import { useNavigate, useLocation } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import { HomeOutlined, DeploymentUnitOutlined, HeatMapOutlined } from '@ant-design/icons'
 
 import styles from './index.module.less'
 
 const ProSecNav = () => {
   const navigate = useNavigate()
-  // const { pathname } = useLocation()
+  const { pathname } = useLocation()
   const redirectTo = (path) => {
     navigate(path)
   }
 
+  const [selectedKeys, setSelectedKeys] = useState(['home'])
+
+  // 当前路由对应的 sub menu key
+  const [openKeys, setOpenKeys] = useState(['home'])
+
+  // submenu keys of first level
+  const [rootSubmenuKeys] = useState(['/sub-act', '/sub-list'])
+
+  useEffect(() => {
+    const selectedPathKey = pathname
+    setSelectedKeys([selectedPathKey])
+    setOpenKeys(openKeys)
+  }, [pathname, openKeys])
+
+  const onItemClick = ({ item, key, keyPath }) => {
+    redirectTo(key)
+  }
+  const onOpenChange = (keys) => {
+    // setOpenKeys(keys)
+    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1)
+    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      setOpenKeys(keys)
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : [])
+    }
+  }
+
   return (
-    <Menu
-      mode="inline"
-      defaultSelectedKeys={['home']}
-      defaultOpenKeys={['act']}
-      // theme="dark"
-      className={styles.menu}
-    >
-      <Menu.Item key="home" icon={<HomeOutlined />} onClick={() => redirectTo('/')}>
-        Home
-      </Menu.Item>
-      <Menu.SubMenu key="demo" icon={<HeatMapOutlined />} title="Demo">
-        <Menu.Item key="sub-demo" icon={<DeploymentUnitOutlined />} onClick={() => redirectTo('demo')}>
+    <>
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={selectedKeys}
+        defaultOpenKeys={openKeys}
+        selectedKeys={selectedKeys}
+        openKeys={openKeys}
+        // theme="dark"
+        className={styles.menu}
+        onOpenChange={onOpenChange}
+        onClick={onItemClick}
+      >
+        <Menu.Item key="/" icon={<HomeOutlined />}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="/demo" icon={<DeploymentUnitOutlined />}>
           Demo
         </Menu.Item>
-      </Menu.SubMenu>
-      <Menu.SubMenu key="act" icon={<HeatMapOutlined />} title="活动单">
-        <Menu.SubMenu key="act-coupons" icon={<DeploymentUnitOutlined />} title="送券活动单">
-          <Menu.Item key="coupons-new" onClick={() => redirectTo('coupons/add')}>
-            新建
-          </Menu.Item>
-          <Menu.Item key="coupons-edit" onClick={() => redirectTo('coupons/edit?id=1')}>
-            编辑
+        <Menu.SubMenu key="/sub-act" icon={<HeatMapOutlined />} title="活动单">
+          <Menu.SubMenu key="/sub-coupons" icon={<DeploymentUnitOutlined />} title="送券活动单">
+            <Menu.Item key="/coupons/add">新建</Menu.Item>
+            <Menu.Item key="/coupons/edit">编辑</Menu.Item>
+          </Menu.SubMenu>
+          <Menu.Item key="/product" icon={<DeploymentUnitOutlined />}>
+            商品调价单
           </Menu.Item>
         </Menu.SubMenu>
-        <Menu.Item key="act-product" icon={<DeploymentUnitOutlined />} onClick={() => redirectTo('product')}>
-          商品调价单
-        </Menu.Item>
-      </Menu.SubMenu>
-    </Menu>
+        <Menu.SubMenu key="/sub-list" icon={<HeatMapOutlined />} title="促销列表">
+          <Menu.Item key="/coupons/list">券列表</Menu.Item>
+          <Menu.Item key="/order/list">订单列表</Menu.Item>
+        </Menu.SubMenu>
+      </Menu>
+    </>
   )
 }
 
