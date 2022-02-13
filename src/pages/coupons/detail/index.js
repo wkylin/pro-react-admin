3ac4f9c2828f93eeeacdbea01701dbddc955069b
wkylin.w <wkylin.w@gmial.com>
2022-02-13 15:31:55 +0800

    feat: useParams

--- src/pages/coupons/detail/index.js
+++ src/pages/coupons/detail/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import { Alert } from 'antd'
 import { useSearchParams } from '@hooks'
 
@@ -6,10 +7,14 @@ const DetailCoupons = () => {
   const query = useSearchParams()
   const term = query.get('id')
 
+  const params = useParams()
+
   return (
     <>
       <Alert message="送券活动单" description="--详情--" type="info" showIcon />
       <span>Search Id: {term}</span>
+
+      <div>useParams: {JSON.stringify(params, null, 2)}</div>
     </>
   )
 }
