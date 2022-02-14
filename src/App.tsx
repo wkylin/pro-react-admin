27e8c8977e1b960f38522b39acdc2be0ca4e83f8
wkylin <wkylin.w@gmail.com>
2022-02-14 10:08:15 +0800

    feat: delete dis

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2021-12-26 14:01:57
- * @LastEditTime: 2022-02-11 18:42:17
- * @LastEditors: your name
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \promotion-web\src\App.tsx
- */
 import React, { useEffect, useState } from 'react'
 // import React, { useEffect, useState, useRef } from 'react'
 
