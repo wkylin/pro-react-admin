77060b06925c6beb7551dbd6cec2be132d726416
wkylin.w <wkylin.w@gmail.com>
2022-03-06 00:10:17 +0800

    feat: matchRoutes

--- src/components/hooks/index.js
+++ src/components/hooks/index.js
@@ -1,5 +1,5 @@
 import { useLocalStorage } from './useLocalStorage'
 import { useUrlSearchParams } from './useUrlSearchParams'
-import { useInterval } from './useInterval'
+import useInterval from './useInterval'
 
 export { useLocalStorage, useUrlSearchParams, useInterval }
