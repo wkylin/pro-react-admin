4045dabac5e32dfd10e8da2fc854f892afae3a06
wkylin.w <wkylin.w@gmail.com>
2022-03-20 06:59:01 +0800

    feat: delete some code

--- src/components/hooks/useForceUpdate/index.js
+++ src/components/hooks/useForceUpdate/index.js
@@ -8,31 +8,4 @@ const useForceUpdate = () => {
   return update
 }
 
-// import store from '../../../store'
-// import { useReducer, useEffect } from 'react'
-
-// const useForceUpdate = () => {
-//   const [, forceUpdateDispatch] = useReducer((v) => v + 1, 0)
-//   useEffect(() => {
-//     const unSubscribeFn = store.subscribe(() => {
-//       forceUpdateDispatch()
-//     })
-//     return unSubscribeFn
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [store])
-//   return store
-// }
-
-// function useForceUpdate() {
-//   const [, forceUpdate] = React.useState()
-
-//   return React.useCallback(() => {
-//     forceUpdate((s) => !s)
-//   }, [])
-// }
-// const forceUpdate = useForceUpdate()
-// const onClick = React.useCallback(() => {
-//   forceUpdate()
-// }, [forceUpdate])
-
 export default useForceUpdate
