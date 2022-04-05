190aadd8a8ff0c1f2ec25bf686363393152429ea
wkylin.w <wkylin.w@gmail.com>
2022-04-05 15:47:37 +0800

    feat: spot light

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,17 @@
 import React, { version } from 'react'
 import FixTabPanel from '@stateless/FixTabPanel'
 import TypedText from '@stateless/TypedText'
+import styles from './index.module.less'
 
 const Home = () => {
   return (
     <FixTabPanel>
-      <h2>
+      <h2 className={styles.typedText}>
         <TypedText>Cool! Hi, React & Ant Design!</TypedText>
       </h2>
-      <h3>React version: {version}</h3>
+      <h3 className={styles.spotLight} data-spotlight={` React version: ${version}`}>
+        React version: {version}
+      </h3>
     </FixTabPanel>
   )
 }
