06b86d6d5b4d92ea5d49277e89bcd3fb013157bc
wkylin.w <wkylin.w@gmail.com>
2022-05-25 23:53:24 +0800

    feat: update redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore, applyMiddleware } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import logger from 'redux-logger'
 import rootReducer from '@src/reducers'
 
-// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-const store = createStore(rootReducer, applyMiddleware(logger))
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: process.env.NODE_ENV !== 'production' ? [logger] : [],
+})
 
 export default store
