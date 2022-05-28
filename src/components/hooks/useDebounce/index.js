830885e3d6810007395942270781a714f75bf40f
wkylin.w <wkylin.w@gmail.com>
2022-05-28 09:39:14 +0800

    feat: use debounce

--- src/components/hooks/useDebounce/index.js
+++ src/components/hooks/useDebounce/index.js
@@ -1,14 +1,54 @@
-const useDebounce = (func, timeout = 1000) => {
-  let timer = null
-  return (...args) => {
-    if (!timer) {
-      func.apply(this.args)
-    }
-    clearTimeout(timer)
-    timer = setTimeout(() => {
-      func.apply(this, args)
-    }, timeout)
-  }
+import { useRef, useEffect, useCallback } from 'react'
+
+const useDebounce = (fn, delay) => {
+  const timerRef = useRef(null)
+  const fnRef = useRef(fn)
+
+  fnRef.current = fn
+
+  useEffect(
+    () => () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current)
+      }
+    },
+    []
+  )
+
+  const fnDebounced = useCallback(
+    function (...args) {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current)
+      }
+      timerRef.current = setTimeout(() => {
+        fnRef.current.apply(this, args)
+      }, delay)
+    },
+    [delay]
+  )
+
+  return fnDebounced
 }
 
 export default useDebounce
+
+// const getRes = useCallback(
+//   (v) => {
+//     fakeFetch({
+//       value: v,
+//       duration,
+//     }).then((res) => {
+//       setRes(res);
+//     });
+//   },
+//   [duration]
+// );
+
+// const getResDebounced = useDebounceFn(getRes, 500);
+
+// const onChange = useCallback(
+//   (e) => {
+//     getResDebounced(e.target.value);
+//   },
+//   [getResDebounced]
+// );
