4d30b86f5bfc2e88a87de286156eafcf4fcf5d20
wkylin.w <wkylin.w@gmail.com>
2022-06-06 22:46:33 +0800

    feat: async fetch

---
+++ src/service/useAsyncFetch.js
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+
+// 1. Fetch on mount
+// 2. Fetch on the button click
+// 3. Fetch on params change
+const useAsyncFetch = (asyncFuntion, args = [], deps = [], immediate = true) => {
+  const isFirstUpdate = useRef(true)
+  const [response, setResponse] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const execute = useCallback(() => {
+    setLoading(true)
+    setResponse(null)
+    setError(null)
+    return asyncFuntion(...args)
+      .then((res) => {
+        setResponse(res)
+      })
+      .catch((err) => {
+        setError(err)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [asyncFuntion, args])
+  useEffect(() => {
+    if (immediate) {
+      execute()
+    } else if (!isFirstUpdate.current) {
+      execute()
+    }
+  }, [...deps])
+  useEffect(() => {
+    isFirstUpdate.current = false
+  }, [])
+  return { execute, response, error, loading }
+}
+
+export default useAsyncFetch
