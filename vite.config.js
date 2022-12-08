6f0418f26d242b2f38fee49bb1363e2b62600f0e
wkylin.w <wkylin.w@gmail.com>
2022-12-08 19:25:22 +0800

    build: fix clean vite config

--- vite.config.js
+++ vite.config.js
@@ -90,6 +90,18 @@ export default defineConfig({
     write: true,
     rollupOptions: {
       // external: [],
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+        // chunkFileNames: (chunkInfo) => {
+        //   const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : []
+        //   const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]'
+        //   return `js/${fileName}/[name].[hash].js`
+        // },
+      },
     },
   },
   preview: {
