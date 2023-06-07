bbf4ec2735c326ddcc8451d4eeed2afe524071dd
wkylin.w <wkylin.w@gmail.com>
2023-06-07 21:21:37 +0800

    feat: remove profiler

--- src/components/stateless/Loading/index.jsx
+++ src/components/stateless/Loading/index.jsx
@@ -3,6 +3,6 @@ import { Spin } from 'antd'
 import { LoadingOutlined } from '@ant-design/icons'
 
 const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />
-const Loading = ({ tip = 'Loading' }) => <Spin indicator={antIcon} tip={tip} />
+const Loading = () => <Spin indicator={antIcon} />
 
 export default Loading
