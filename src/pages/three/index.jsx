3adbfb1a1df003bd4882d2d493739bf749892ed6
wkylin.w <wkylin.w@gmail.com>
2023-06-19 17:27:31 +0800

    feat: react three fiber

--- src/pages/three/index.jsx
+++ src/pages/three/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Points, PointMaterial } from '@react-three/drei'
 import * as random from 'maath/random/dist/maath-random.esm'
