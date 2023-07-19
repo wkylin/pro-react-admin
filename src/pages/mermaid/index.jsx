12cc66bff14737bb11cb4e65bbbe1b4e92f5b18c
wkylin.w <wkylin.w@gmail.com>
2023-07-19 23:04:21 +0800

    feat: mermaid

--- src/pages/mermaid/index.jsx
+++ src/pages/mermaid/index.jsx
@@ -13,36 +13,6 @@ const gitChart = `gitGraph
   commit
   commit
 `
-
-const diagramChart = `
----
-title: Animal example
----
-classDiagram
-    note "From Duck till Zebra"
-    Animal <|-- Duck
-    note for Duck "can fly\ncan swim\ncan dive\ncan help in debugging"
-    Animal <|-- Fish
-    Animal <|-- Zebra
-    Animal : +int age
-    Animal : +String gender
-    Animal: +isMammal()
-    Animal: +mate()
-    class Duck{
-        +String beakColor
-        +swim()
-        +quack()
-    }
-    class Fish{
-        -int sizeInFeet
-        -canEat()
-    }
-    class Zebra{
-        +bool is_wild
-        +run()
-    }
-`
-
 const Mermaid = () => (
   <>
     <FixTabPanel>
@@ -50,9 +20,6 @@ const Mermaid = () => (
 
       <h3>演示1:Git Diagram </h3>
       <MermaidHooks chart={gitChart} />
-
-      <h3>演示2:Class Diagram</h3>
-      <MermaidHooks chart={diagramChart} />
     </FixTabPanel>
   </>
 )
