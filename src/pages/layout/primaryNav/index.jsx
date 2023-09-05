99b9d8a1c98a3e809ff32a8108f55351e7e16675
wkylin.w <wkylin.w@gmail.com>
2023-09-05 22:20:17 +0800

    feat: update pkg

--- src/pages/layout/primaryNav/index.jsx
+++ src/pages/layout/primaryNav/index.jsx
@@ -8,7 +8,7 @@ const PrimaryNav = () => {
   const redirectTo = (path) => {
     navigate(path)
   }
-  const goToSkylin = () => {
+  const goToWkylin = () => {
     window.open(`https://skyline.github.com/wkylin/${new Date().getFullYear() - 1}`, '_blank')
   }
 
@@ -18,7 +18,7 @@ const PrimaryNav = () => {
         <Button type="link" icon={<CodeOutlined />} onClick={() => redirectTo('dashboard')}>
           多路由设置
         </Button>
-        <Button type="link" icon={<TeamOutlined />} onClick={goToSkylin}>
+        <Button type="link" icon={<TeamOutlined />} onClick={goToWkylin}>
           Skyline for wkylin.w
         </Button>
       </Space>
--- src/pages/tilt/index.jsx
+++ src/pages/tilt/index.jsx
@@ -1,33 +1,17 @@
 import React from 'react'
 import FixTabPanel from '@stateless/FixTabPanel'
-import { Tilt } from 'react-tilt'
-import wkylinPng from '@assets/images/wkylin.png'
+import Tilt from 'react-parallax-tilt'
 
-const defaultOptions = {
-  reverse: false, // reverse the tilt direction
-  max: 35, // max tilt rotation (degrees)
-  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-  scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
-  speed: 1000, // Speed of the enter/exit transition
-  transition: true, // Set a transition on enter/exit.
-  axis: null, // What axis should be disabled. Can be X or Y.
-  reset: true, // If the tilt effect has to be reset on exit.
-  easing: 'cubic-bezier(.03,.98,.52,.99)', // Easing on enter/exit.
-}
+import wkylinPng from '@assets/images/wkylin.png'
 
 const ReactTilt = () => (
   <FixTabPanel>
     <h3>Welcome to react tilt!</h3>
-    <Tilt
-      options={defaultOptions}
-      className="bg-[#55467c] p-2 rounded-2xl w-full flex items-center justify-center"
-      style={{ height: 248, width: 248 }}
-    >
-      <div className="text-white">
-        <img src={wkylinPng} alt="wkylin.w" />
-        <h2>wkylin.w</h2>
-      </div>
-    </Tilt>
+    <section style={{ width: 200, height: 200 }}>
+      <Tilt tiltMaxAngleX={40} tiltMaxAngleY={40} perspective={1000} scale={1.1} glareEnable={true}>
+        <img src={wkylinPng} alt="wkylin.w" width="200" height="200" />
+      </Tilt>
+    </section>
   </FixTabPanel>
 )
 
