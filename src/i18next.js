f05d09cd0e18f132d611d5906127c9f99806f9db
wkylin.w <wkylin.w@gmail.com>
2023-11-14 16:29:46 +0800

    feat: 国际化

--- src/i18next.js
+++ src/i18next.js
@@ -1,19 +1,35 @@
 import i18n from 'i18next'
-import Backend from 'i18next-http-backend'
+// import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
 i18n
-  .use(Backend)
+  // .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    // backend: {
+    //   loadPath: `${process.env.PUBLIC_URL}/locales/{{lng}}/{{ns}}.json`,
+    // },
+    resources: {
+      en: {
+        translation: {
+          demo: 'Demo',
+          lang: 'En',
+          home: 'Home',
+        },
+      },
+      zh: {
+        translation: {
+          demo: '演示',
+          lang: 'ZH',
+          home: '首页',
+        },
+      },
     },
     lng: 'zh',
-    fallbackLng: 'zh', // 默认语言
-    debug: process.env.NODE_ENV === 'production', // 开启调试模式
+    fallbackLng: 'en', // 默认语言
+    debug: process.env.NODE_ENV !== 'production', // 开启调试模式
     interpolation: {
       escapeValue: false, // 不转义特殊字符
     },
