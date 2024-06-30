072fdf2bbe3e4b4c90d68095bccb4d3e09b3fd1c
wkylin.w <wkylin.w@gmail.com>
2024-06-30 18:28:40 +0800

    Updates

---
+++ static/js/420.ec8d26d208d164b5a6ad.js
@@ -0,0 +1,3 @@
+"use strict";(self.webpackChunkpro_react_admin=self.webpackChunkpro_react_admin||[]).push([[420],{5420:function(i,a,e){var u=e(4994);Object.defineProperty(a,"__esModule",{value:!0}),a.default=void 0;var t=u(e(6540)),n=e(1080),l=e(3044),r=function(){var o=(0,n.useLocation)(),c=(0,n.useNavigate)();return t.default.createElement("div",{style:{margin:30,textAlign:"center"}},t.default.createElement("h3",null,"No match for ",t.default.createElement("code",null,o.pathname)),t.default.createElement(l.Button,{size:"small",type:"primary",ghost:!0,onClick:function(){return c(-1)}},"\u8FD4\u56DE"))},d=a.default=r}}]);
+
+//# sourceMappingURL=420.ec8d26d208d164b5a6ad.js.map
---
+++ static/js/885.3f00ca23139d5bf7357a.js
@@ -0,0 +1,17 @@
+(self.webpackChunkpro_react_admin=self.webpackChunkpro_react_admin||[]).push([[885],{9480:function(a,s,e){"use strict";var _=e(1354),l=e.n(_),t=e(6314),o=e.n(t),n=o()(l());n.push([a.id,`.alignCenter--aqiSR {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+`,"",{version:3,sources:["webpack://./src/components/stateless/AlignCenter/index.module.less"],names:[],mappings:"AAAA;EACE,WAAW;EACX,iBAAiB;EACjB,aAAa;EACb,mBAAmB;EACnB,uBAAuB;AACzB",sourcesContent:[`.alignCenter {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+`],sourceRoot:""}]),n.locals={alignCenter:"alignCenter--aqiSR"},s.A=n},304:function(a,s,e){"use strict";var _=e(4994);Object.defineProperty(s,"__esModule",{value:!0}),s.default=void 0;var l=_(e(6540)),t=_(e(1793)),o=function(d){var u=d.children;return l.default.createElement("div",{className:t.default.alignCenter},u)},n=s.default=o},9885:function(a,s,e){"use strict";var _=e(4994);Object.defineProperty(s,"__esModule",{value:!0}),s.default=void 0;var l=_(e(4634)),t=_(e(6540)),o=e(1080),n=e(3044),m=_(e(304)),d=e(1265),u=n.Layout.Content,c={labelCol:{span:8},wrapperCol:{span:16}},E={wrapperCol:{offset:8,span:16}},v=function(){var r=(0,o.useNavigate)(),y=function(f){r(f)},C=n.theme.useToken(),M=C.token.colorBgContainer,O=function(f){var g=f.username;(0,d.setLocalStorage)("token",{token:g}),y("/")},D=function(f){};return t.default.createElement(n.Layout,{style:{height:"100%"}},t.default.createElement(u,{style:{height:"100%",background:M}},t.default.createElement(m.default,null,t.default.createElement(n.Form,(0,l.default)({},c,{name:"basic",initialValues:{remember:!0,username:"wkylin.w",password:"wkylin.w"},onFinish:O,onFinishFailed:D}),t.default.createElement(n.Form.Item,{label:"Username",name:"username",rules:[{required:!0,message:"Please input your username!"}]},t.default.createElement(n.Input,null)),t.default.createElement(n.Form.Item,{label:"Password",name:"password",rules:[{required:!0,message:"Please input your password!"}]},t.default.createElement(n.Input.Password,null)),t.default.createElement(n.Form.Item,(0,l.default)({},E,{name:"remember",valuePropName:"checked"}),t.default.createElement(n.Checkbox,null,"Remember me")),t.default.createElement(n.Form.Item,E,t.default.createElement(n.Button,{type:"primary",htmlType:"submit"}," ","Sign in"))))))},A=s.default=v},1793:function(a,s,e){"use strict";e.r(s);var _=e(5072),l=e.n(_),t=e(7825),o=e.n(t),n=e(7659),m=e.n(n),d=e(5056),u=e.n(d),c=e(540),E=e.n(c),v=e(1113),A=e.n(v),i=e(9480),r={};r.styleTagTransform=A(),r.setAttributes=u(),r.insert=m().bind(null,"head"),r.domAPI=o(),r.insertStyleElement=E();var y=l()(i.A,r);s.default=i.A&&i.A.locals?i.A.locals:void 0},4634:function(a){function s(){return a.exports=s=Object.assign?Object.assign.bind():function(e){for(var _=1;_<arguments.length;_++){var l=arguments[_];for(var t in l)({}).hasOwnProperty.call(l,t)&&(e[t]=l[t])}return e},a.exports.__esModule=!0,a.exports.default=a.exports,s.apply(null,arguments)}a.exports=s,a.exports.__esModule=!0,a.exports.default=a.exports}}]);
+
+//# sourceMappingURL=885.3f00ca23139d5bf7357a.js.map
