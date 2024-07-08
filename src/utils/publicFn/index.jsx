5a39e3ecb8e05e740db7820e41f71eb2ea4dd3dd
wkylin.w <wkylin.w@gmail.com>
2024-07-08 20:59:04 +0800

    feat: 大屏可视化

--- src/utils/publicFn/index.jsx
+++ src/utils/publicFn/index.jsx
@@ -64,22 +64,14 @@ export const formatTime = (time, fmt) => {
     'm+': date.getMinutes(),
     's+': date.getSeconds(),
     'q+': Math.floor((date.getMonth() + 3) / 3),
-    S: date.getMilliseconds()
+    S: date.getMilliseconds(),
   }
   if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(
-      RegExp.$1,
-      (date.getFullYear() + '').substr(4 - RegExp.$1.length)
-    )
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
   }
   for (const k in o) {
     if (new RegExp('(' + k + ')').test(fmt)) {
-      fmt = fmt.replace(
-        RegExp.$1,
-        RegExp.$1.length === 1
-          ? o[k].toString()
-          : ('00' + o[k]).substr(('' + o[k]).length)
-      )
+      fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k].toString() : ('00' + o[k]).substr(('' + o[k]).length))
     }
   }
   return fmt
