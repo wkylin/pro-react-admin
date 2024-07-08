565383a33f756d3ce25ba1f3c2e4cc46f8c7edfa
wkylin.w <wkylin.w@gmail.com>
2024-07-08 16:22:51 +0800

    Updates

---
+++ static/js/739.b658ef3e173e49d9ff5e.js
@@ -0,0 +1,17 @@
+(self.webpackChunkpro_react_admin=self.webpackChunkpro_react_admin||[]).push([[739],{69480:function(l,s,e){"use strict";var _=e(71354),a=e.n(_),t=e(76314),o=e.n(t),n=o()(a());n.push([l.id,`.alignCenter--aqiSR {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+`,"",{version:3,sources:["webpack://./src/components/stateless/AlignCenter/index.module.less"],names:[],mappings:"AAAA;EACE,WAAW;EACX,iBAAiB;EACjB,aAAa;EACb,mBAAmB;EACnB,uBAAuB;AACzB",sourcesContent:[`.alignCenter {
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+`],sourceRoot:""}]),n.locals={alignCenter:"alignCenter--aqiSR"},s.A=n},20304:function(l,s,e){"use strict";var _=e(24994);Object.defineProperty(s,"__esModule",{value:!0}),s.default=void 0;var a=_(e(96540)),t=_(e(61793)),o=function(d){var i=d.children;return a.default.createElement("div",{className:t.default.alignCenter},i)},n=s.default=o},43739:function(l,s,e){"use strict";var _=e(24994);Object.defineProperty(s,"__esModule",{value:!0}),s.default=void 0;var a=_(e(94634)),t=_(e(96540)),o=e(11080),n=e(2147),m=_(e(20304)),d=n.Layout.Content,i={labelCol:{span:8},wrapperCol:{span:16}},E={wrapperCol:{offset:8,span:16}},f=function(){var u=(0,o.useNavigate)(),r=function(v){u(v)},P=n.theme.useToken(),y=P.token.colorBgContainer,M=function(v){},O=function(v){};return t.default.createElement(n.Layout,{style:{height:"100%"}},t.default.createElement(d,{style:{height:"100%",background:y}},t.default.createElement(m.default,null,t.default.createElement(n.Form,(0,a.default)({},i,{name:"basic",initialValues:{remember:!0},onFinish:M,onFinishFailed:O}),t.default.createElement(n.Form.Item,{label:"Username",name:"username",rules:[{required:!0,message:"Please input your username!"}]},t.default.createElement(n.Input,null)),t.default.createElement(n.Form.Item,{label:"Password",name:"password",rules:[{required:!0,message:"Please input your password!"}]},t.default.createElement(n.Input.Password,null)),t.default.createElement(n.Form.Item,(0,a.default)({},E,{name:"remember",valuePropName:"checked"}),t.default.createElement(n.Checkbox,null,"Remember me")),t.default.createElement(n.Form.Item,E,t.default.createElement(n.Button,{type:"primary",onClick:function(){return r("/")}},"Sign up"))))))},A=s.default=f},61793:function(l,s,e){"use strict";e.r(s);var _=e(85072),a=e.n(_),t=e(97825),o=e.n(t),n=e(77659),m=e.n(n),d=e(55056),i=e.n(d),E=e(10540),f=e.n(E),A=e(41113),C=e.n(A),u=e(69480),r={};r.styleTagTransform=C(),r.setAttributes=i(),r.insert=m().bind(null,"head"),r.domAPI=o(),r.insertStyleElement=f();var P=a()(u.A,r);s.default=u.A&&u.A.locals?u.A.locals:void 0},94634:function(l){function s(){return l.exports=s=Object.assign?Object.assign.bind():function(e){for(var _=1;_<arguments.length;_++){var a=arguments[_];for(var t in a)({}).hasOwnProperty.call(a,t)&&(e[t]=a[t])}return e},l.exports.__esModule=!0,l.exports.default=l.exports,s.apply(null,arguments)}l.exports=s,l.exports.__esModule=!0,l.exports.default=l.exports}}]);
+
+//# sourceMappingURL=739.b658ef3e173e49d9ff5e.js.map
