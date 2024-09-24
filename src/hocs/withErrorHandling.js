f42d209e7d6e5c80bb7b1e9c62b8ba98bc609419
wkylin <wkylin.w@gmail.com>
2024-09-24 14:24:13 +0800

    feat: hooks hocs

---
+++ src/hocs/withErrorHandling.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function withErrorHandling(Component) {
+  return function WithErrorHandling({ error, ...props }) {
+    if (error) {
+      return <div>Error: {error.message}</div>
+    }
+    return <Component {...props} />
+  }
+}
+
+export default withErrorHandling
+
+// const ListWithLoading = withLoading(List);
+// const ListWithErrorHandling = withErrorHandling(ListWithLoading);
---
+++ src/hocs/withLoading.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function withLoading(Component) {
+  return function WithLoading({ isLoading, ...props }) {
+    if (isLoading) {
+      return <div>Loading...</div>
+    }
+    return <Component {...props} />
+  }
+}
+
+export default withLoading
