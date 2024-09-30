29acfe6d0335403d231d8a70eccf0587602009ff
wkylin <wkylin.w@gmail.com>
2024-09-30 15:42:49 +0800

    chore: style lint

--- src/utils/publicFn/index.jsx
+++ src/utils/publicFn/index.jsx
@@ -54,27 +54,23 @@ export const clearLocalStorage = () => {
  * @param {Date} time 需要转换的时间
  * @param {string} fmt 需要转换的格式 如 yyyy-MM-dd、yyyy-MM-dd HH:mm:ss
  */
-export const formatTime = (time, fmt) => {
-  if (!time) return ''
-  const date = new Date(time)
-  const o = {
-    'M+': date.getMonth() + 1,
-    'd+': date.getDate(),
-    'H+': date.getHours(),
-    'm+': date.getMinutes(),
-    's+': date.getSeconds(),
-    'q+': Math.floor((date.getMonth() + 3) / 3),
-    S: date.getMilliseconds(),
+export const formatTime = (date, format) => {
+  if (!(date instanceof Date)) {
+    throw new Error('Invalid date input')
   }
-  if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+
+  const padZero = (num, len = 2) => num.toString().padStart(len, '0')
+
+  const map = {
+    yyyy: date.getFullYear(),
+    MM: padZero(date.getMonth() + 1),
+    dd: padZero(date.getDate()),
+    HH: padZero(date.getHours()),
+    mm: padZero(date.getMinutes()),
+    ss: padZero(date.getSeconds()),
   }
-  for (const k in o) {
-    if (new RegExp('(' + k + ')').test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? o[k].toString() : ('00' + o[k]).substr(('' + o[k]).length))
-    }
-  }
-  return fmt
+
+  return format.replace(/yyyy|MM|dd|HH|mm|ss/g, (match) => map[match])
 }
 
 /**
@@ -99,7 +95,7 @@ export const toThousands = (num) => {
   let result = ''
   let numStr = (num || 0).toString()
   while (numStr.length > 3) {
-    result = ',' + numStr.slice(-3) + result
+    result = `,${numStr.slice(-3)}${result}`
     numStr = numStr.slice(0, numStr.length - 3)
   }
   if (numStr) {
