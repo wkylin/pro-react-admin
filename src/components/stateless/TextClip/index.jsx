3a36d45c2c29d0ac43280695bc3ba7366e718e4f
wkylin <wkylin.w@gmail.com>
2024-12-02 09:44:59 +0800

    chore: sentry cli

---
+++ src/components/stateless/TextClip/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const TextClips = () => {
+
+  return (
+    <div className="elementor-widget-container">
+      <h1 className="elementor-headline elementor-headline-animation-type-clip">
+        <span className="elementor-headline-plain-text elementor-headline-text-wrapper">Trending</span>
+        <span className="elementor-headline-dynamic-wrapper elementor-headline-text-wrapper" style="width: 20.9824px; overflow: hidden;">
+          <span className="elementor-headline-dynamic-text elementor-headline-text-inactive">Bootstrap</span>
+          <span className="elementor-headline-dynamic-text elementor-headline-text-active">React</span>
+          <span className="elementor-headline-dynamic-text elementor-headline-text-inactive">Vue</span>
+          <span className="elementor-headline-dynamic-text elementor-headline-text-inactive">Angular</span>
+          <span className="elementor-headline-dynamic-text elementor-headline-text-inactive">Django</span>
+          <span className="elementor-headline-dynamic-text elementor-headline-text-inactive">Flask</span>
+          <span className="elementor-headline-dynamic-text elementor-headline-text-inactive">Laravel</span>
+          <span className="elementor-headline-dynamic-text elementor-headline-text-inactive">Ul&nbsp;Kit</span>
+          <span className="elementor-headline-dynamic-text elementor-headline-text-inactive">Freebies</span>
+          </span>
+        <span className="elementor-headline-plain-text elementor-headline-text-wrapper">Templates</span>
+      </h1>
+		</div>
+  )
+}
+
+export default TextClips
