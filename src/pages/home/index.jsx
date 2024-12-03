c8cea37e5e522d955885b7113dd2c5f526daac68
wkylin <wkylin.w@gmail.com>
2024-12-03 09:38:03 +0800

    feat: spot light

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -242,12 +242,12 @@ const Home = () => {
           </>
         </AnimateOnScreen.ScaleIn>
       </section>
-      {/* <section style={{ marginBottom: 40, width: 360, height: 200 }}>
-        <Spotlight className="w-full h-full">
+      <section style={{ marginBottom: 40, width: 360, height: 200 }}>
+        <Spotlight className="w-full h-full group mx-auto grid max-w-sm items-start gap-6">
           <div className="relative w-full h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-900/90 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 after:hover:opacity-20 before:group-hover:opacity-100">
           </div>
         </Spotlight>
-      </section> */}
+      </section>
       <section style={{ marginBottom: 40, height: 200, width: 360, overflow: 'hidden' }}>
         <MeshGradientBackground />
       </section>
