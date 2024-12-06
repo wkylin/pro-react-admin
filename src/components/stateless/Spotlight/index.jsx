1f73a4191cc49038c403afa52f3109566c869f1c
wkylin <wkylin.w@gmail.com>
2024-12-06 15:21:47 +0800

    feat: spotlight border

--- src/components/stateless/Spotlight/index.jsx
+++ src/components/stateless/Spotlight/index.jsx
@@ -1,7 +1,7 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState } from 'react'
 import styles from './index.module.less'
 
-const SpotlightCard = ({className, style,children}) => {
+const SpotlightCard = ({className, style, children}) => {
   const divRef = useRef(null)
   const [isFocused, setIsFocused] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
@@ -43,13 +43,24 @@ const SpotlightCard = ({className, style,children}) => {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       className={`${styles.cardEffect} ${className}`}
-      style={{...style}}
+      style={{
+        ...style,
+      }}
     >
       <section
         className={styles.spotlight}
         style={{
           opacity,
           background: `radial-gradient(1000px circle at ${position.x}px ${position.y}px, rgba(190,255,255,.2), transparent 40%)`,
+        }}
+      />
+      <section
+        className={styles.spotlight}
+        style={{
+          opacity,
+          border: "2px solid rgb(255, 255, 255)",
+          borderRadius: '8px',
+          WebkitMaskImage: `radial-gradient(40% 40% at ${position.x}px ${position.y}px, black 45%, transparent)`
         }}
       />
       {children}
