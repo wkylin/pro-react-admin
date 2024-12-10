ea7f6983b0bc8fc63850d7156bd4d29c190e9f5f
wkylin <wkylin.w@gmail.com>
2024-12-10 18:08:00 +0800

    Updates

---
+++ static/js/1274.9bb644eb581f9eec5c41.js
@@ -0,0 +1,69 @@
+"use strict";(self.webpackChunkpro_react_admin=self.webpackChunkpro_react_admin||[]).push([[1274],{42706:function(c,_,e){var a=e(71354),i=e.n(a),r=e(76314),o=e.n(r),d=o()(i());d.push([c.id,`.warpper--O5ESm {\r
+  font-family: sans-serif;\r
+  text-align: center;\r
+}\r
+\r
+.pre--csWZr {\r
+  text-align: left;\r
+  margin: 1em 0;\r
+  padding: 0.5em;\r
+  overflow: auto;\r
+\r
+  & .token-line--Oslmy {\r
+    line-height: 1.3em;\r
+    height: 1.3em;\r
+  }\r
+}\r
+\r
+.line--wTVoy {\r
+  padding-right: 0.5em;\r
+}\r
+\r
+.line-no--diNU0 {\r
+  text-align: right;\r
+  padding-right: 1em;\r
+  -webkit-user-select: none;\r
+      -ms-user-select: none;\r
+          user-select: none;\r
+  opacity: 0.5;\r
+}\r
+`,"",{version:3,sources:["webpack://./src/pages/prism/index.module.less"],names:[],mappings:"AAAA;EACE,uBAAuB;EACvB,kBAAkB;AACpB;;AAEA;EACE,gBAAgB;EAChB,aAAa;EACb,cAAc;EACd,cAAc;;EAEd;IACE,kBAAkB;IAClB,aAAa;EACf;AACF;;AAEA;EACE,oBAAoB;AACtB;;AAEA;EACE,iBAAiB;EACjB,kBAAkB;EAClB,yBAAiB;MAAjB,qBAAiB;UAAjB,iBAAiB;EACjB,YAAY;AACd",sourcesContent:[`.warpper {\r
+  font-family: sans-serif;\r
+  text-align: center;\r
+}\r
+\r
+.pre {\r
+  text-align: left;\r
+  margin: 1em 0;\r
+  padding: 0.5em;\r
+  overflow: auto;\r
+\r
+  & .token-line {\r
+    line-height: 1.3em;\r
+    height: 1.3em;\r
+  }\r
+}\r
+\r
+.line {\r
+  padding-right: 0.5em;\r
+}\r
+\r
+.line-no {\r
+  text-align: right;\r
+  padding-right: 1em;\r
+  user-select: none;\r
+  opacity: 0.5;\r
+}\r
+`],sourceRoot:""}]),d.locals={warpper:"warpper--O5ESm",pre:"pre--csWZr","token-line":"token-line--Oslmy",tokenLine:"token-line--Oslmy",line:"line--wTVoy","line-no":"line-no--diNU0",lineNo:"line-no--diNU0"},_.A=d},42916:function(c,_,e){var a=e(24994);Object.defineProperty(_,"__esModule",{value:!0}),_.default=void 0;var i=a(e(43693)),r=a(e(96540));function o(l,s){var n=Object.keys(l);if(Object.getOwnPropertySymbols){var t=Object.getOwnPropertySymbols(l);s&&(t=t.filter(function(E){return Object.getOwnPropertyDescriptor(l,E).enumerable})),n.push.apply(n,t)}return n}function d(l){for(var s=1;s<arguments.length;s++){var n=arguments[s]!=null?arguments[s]:{};s%2?o(Object(n),!0).forEach(function(t){(0,i.default)(l,t,n[t])}):Object.getOwnPropertyDescriptors?Object.defineProperties(l,Object.getOwnPropertyDescriptors(n)):o(Object(n)).forEach(function(t){Object.defineProperty(l,t,Object.getOwnPropertyDescriptor(n,t))})}return l}var u=function(s){var n=s.style,t=s.children;return r.default.createElement("div",{style:d({width:"100%",minHeight:"calc(100vh - 232px)"},n)},t)},f=_.default=u},61274:function(c,_,e){var a=e(24994);Object.defineProperty(_,"__esModule",{value:!0}),_.default=void 0;var i=a(e(94634)),r=a(e(96540)),o=e(77450),d=a(e(42916)),u=a(e(19417)),f=`
+const GroceryItem: React.FC<GroceryItemProps> = ({ item }) => {
+  return (
+    <div>
+      <h2>{item.name}</h2>
+      <p>Price: {item.price}</p>
+      <p>Quantity: {item.quantity}</p>
+    </div>
+  );
+}
+`.trim(),l=function(){return r.default.createElement(d.default,null,r.default.createElement("h3",null,"Welcome to prism-react-renderer!"),r.default.createElement(o.Highlight,{theme:o.themes.dracula,className:u.default.wrapper,code:f,language:"jsx"},function(t){var E=t.style,A=t.tokens,m=t.getLineProps,y=t.getTokenProps;return r.default.createElement("pre",{className:u.default.pre,style:E},A.map(function(O,P){return r.default.createElement("div",(0,i.default)({className:u.default.line,key:P},m({line:O})),r.default.createElement("span",{className:u.default["line-no"]},P+1),O.map(function(p,v){return r.default.createElement("span",(0,i.default)({key:v},y({token:p})))}))}))}))},s=_.default=l},19417:function(c,_,e){e.r(_);var a=e(85072),i=e.n(a),r=e(97825),o=e.n(r),d=e(77659),u=e.n(d),f=e(55056),l=e.n(f),s=e(10540),n=e.n(s),t=e(41113),E=e.n(t),A=e(42706),m={};m.styleTagTransform=E(),m.setAttributes=l(),m.insert=u().bind(null,"head"),m.domAPI=o(),m.insertStyleElement=n();var y=i()(A.A,m);_.default=A.A&&A.A.locals?A.A.locals:void 0}}]);
+
+//# sourceMappingURL=1274.9bb644eb581f9eec5c41.js.map
