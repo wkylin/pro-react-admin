cf3f9e1397a2fd40cbd6cb78d1ae0fd8b35a645b
wkylin.w@gmail.com <wkylin.w@gmail.com>
2024-12-24 20:31:11 +0800

    Updates

---
+++ static/js/420.6849a75f7a083df31137.js
@@ -0,0 +1,3 @@
+"use strict";(self.webpackChunkpro_react_admin=self.webpackChunkpro_react_admin||[]).push([[420],{65420:function(i,a,e){var u=e(24994);Object.defineProperty(a,"__esModule",{value:!0}),a.default=void 0;var t=u(e(96540)),n=e(84976),l=e(58087),r=function(){var o=(0,n.useLocation)(),c=(0,n.useNavigate)();return t.default.createElement("div",{style:{margin:30,textAlign:"center"}},t.default.createElement("h3",null,"No match for ",t.default.createElement("code",null,o.pathname)),t.default.createElement(l.Button,{size:"small",type:"primary",ghost:!0,onClick:function(){return c(-1)}},"\u8FD4\u56DE"))},d=a.default=r}}]);
+
+//# sourceMappingURL=420.6849a75f7a083df31137.js.map
