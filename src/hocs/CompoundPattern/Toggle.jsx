1a55402b61005c1879fc5c2818b6ef7c91ddc4fc
wkylin.w@gmail.com <wkylin.w@gmail.com>
2025-02-21 11:39:20 +0800

    feat: compound pattern

---
+++ src/hocs/CompoundPattern/Toggle.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Toggle = ({ children }) => {
+  const [on, setOn] = useState(false)
+  const toggle = () => {
+    setOn(!on)
+  }
+  const getChildProps = () => {
+    return {
+      on,
+      toggle,
+    }
+  }
+  return <>{React.Children.map(children, (child) => React.cloneElement(child, getChildProps()))}</>
+}
+export default Toggle
---
+++ src/hocs/CompoundPattern/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Toggle from './Toggle'
+import ToggleButton from './ToggleButton'
+import ToggleMessage from './ToggleMessage'
+const ComPattern = () => (
+  <Toggle>
+    <ToggleButton />
+    <ToggleMessage />
+  </Toggle>
+)
+export default ComPattern
