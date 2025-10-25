f5310f123d8531c7e333a7326ff34adc19628b63
wkylin.w <wkylin.w@gmail.com>
2025-10-25 10:27:19 +0800

    Updates

---
+++ static/js/react-syntax-highlighter_languages_highlight_brainfuck.1eb5e5b933ee695ced62.js
@@ -0,0 +1,3 @@
+(self.webpackChunkpro_react_admin=self.webpackChunkpro_react_admin||[]).push([[6835],{13476:(function(n){function a(r){const e={className:"literal",begin:/[+-]/,relevance:0};return{name:"Brainfuck",aliases:["bf"],contains:[r.COMMENT(`[^\\[\\]\\.,\\+\\-<> \r
+]`,`[\\[\\]\\.,\\+\\-<> \r
+]`,{returnEnd:!0,relevance:0}),{className:"title",begin:"[\\[\\]]",relevance:0},{className:"string",begin:"[\\.,]",relevance:0},{begin:/(?:\+\+|--)/,contains:[e]},e]}}n.exports=a})}]);
